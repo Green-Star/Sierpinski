2b555495b52fb6780b00f89eb23a14918aa90bc6
Green-Star <alexandre.sahuc@cpy.re>
2018-01-02 19:42:26 +0100

    Add initialization

--- C/src/sierpinski.c
+++ C/src/sierpinski.c
@@ -5,6 +5,21 @@
 #define NO_CHAR ' '
 #define IS_CHAR 'O'
 
+/* From https://stackoverflow.com/questions/101439/the-most-efficient-way-to-implement-an-integer-based-power-function-powint-int */
+static int ipow(int base, int exp)
+{
+    int result = 1;
+    while (exp)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        base *= base;
+    }
+
+    return result;
+}
+
 static inline char get_sierpinski_char(char current, char above) {
   return (above == NO_CHAR) ? NO_CHAR : current;
 }
@@ -27,12 +42,30 @@ void do_sierpinski(int level, int depth, char c) {
 }
 
 int main(int argc, char **argv) {
-  int depth = 3;
+  struct bmp_file file;
+  struct bmp_pixel sierpinski_pixel, background_pixel;
+  int depth;
+  int nb_pixels;
+
+  if (argc < 3) {
+    fprintf(stderr, "Usage : %s <image_name> <depth>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  sierpinski_pixel.r = sierpinski_pixel.g = sierpinski_pixel.b = 255;
+  background_pixel.r = background_pixel.g = background_pixel.b = 0;
+
+  depth = strtol(argv[2], NULL, 10);
+  nb_pixels = ipow(3, depth);
+
+  bmp_start(argv[1], nb_pixels, nb_pixels, &file);
 
   if (depth > 0) {
     do_sierpinski(1, depth, IS_CHAR);
     printf("\n");
   }
+
+  bmp_end(file);
 
   return EXIT_SUCCESS;
 }
